b26bfa45f6d77284f732544db236d40757e2b812
Milos252 <77508044+Milos252@users.noreply.github.com>
2022-11-26 16:19:35 +0100

    Withdraw and deposit

--- bank/bank.js
+++ bank/bank.js
@@ -1,8 +1,8 @@
 let url = 'https://62a66ad3430ba53411d48b32.mockapi.io/accounts/';
+let money;
 
 let id = sessionStorage.getItem('id');
-let loggedIn = (sessionStorage.getItem('loggedIn') === 'true');
-
+let loggedIn = sessionStorage.getItem('loggedIn') === 'true';
 
 if (loggedIn !== true) {
     window.location.href = '../index.html';
@@ -16,7 +16,50 @@ fetch(url + id)
         let spanPassword = document.querySelector('#password');
         let spanEmail = document.querySelector('#email');
         p.innerText = data.money + '$';
+        money = parseInt(data.money);
+
         spanName.innerText = data.name;
         spanPassword.innerText = data.password;
         spanEmail.innerText = data.email;
     });
+
+let deposit = document.querySelector('#deposit-btn');
+
+let setMoney = () => {
+    fetch(url + id, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            money: money,
+        }),
+    });
+};
+
+let setDisplay = () => {
+    fetch(url + id)
+        .then((response) => response.json())
+        .then(() => {
+            let p = document.querySelector('#money-text');
+            p.innerText = money + '$';
+        });
+};
+
+deposit.addEventListener('click', () => {
+    let depositInput = parseInt(document.querySelector('#deposit-input').value);
+    money += depositInput;
+    setMoney();
+    setDisplay();
+});
+
+let withdraw = document.querySelector('#withdraw-btn');
+
+withdraw.addEventListener('click', () => {
+    let withdrawInput = parseInt(
+        document.querySelector('#withdraw-input').value
+    );
+    money -= withdrawInput;
+    setMoney();
+    setDisplay();
+});
